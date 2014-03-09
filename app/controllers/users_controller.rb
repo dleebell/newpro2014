fcc5d635094f49edfce13e6e52095d0d170356e7
(Dominic) <(Dleecampbell@hotmail.co.uk)>
2014-03-09 13:24:50 +0000

    Finish user signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,23 @@
 class UsersController < ApplicationController
+def show
+	@User.find(params[:id])
+end
   def new
+  	@user = User.new
+  def create 
+  	@user = User.new(user_params) # Not the final implemention!
+  	if @user.save
+      redirect_to @user
+  		#Handle a successful save
+  	else
+  		render 'new'
+  	end
+  end
+
+  private
+
+  def user_params 
+  	params.require(:user).permit(:name, :email, :password,
+  		:password_confirmation)
   end
 end
